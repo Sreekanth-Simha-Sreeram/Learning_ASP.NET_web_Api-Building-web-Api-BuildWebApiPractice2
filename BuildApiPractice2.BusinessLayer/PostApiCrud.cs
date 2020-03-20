a3b1a0c4fdeb5dc144bed44c618c60a9cf5e7cbb
Jesuraj77 <isysraju2001@gmail.com>
2020-03-20 13:52:49 +0530

    Code Added

---
+++ BuildApiPractice2.BusinessLayer/PostApiCrud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BuildApiPractice2.BusinessLayer
+{
+  public class PostApiCrud
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post(string value)
+        {
+            //create a resource and return URL of the resource. 
+
+
+        }
+    }
+}
---
+++ BuildWebApiPractice2/Controllers/PostApiCrudController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BuildWebApiPractice2.Controllers
+{
+    public class PostApiCrudController : ApiController
+    {
+        
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            //create a resource and return URL of the resource. 
+
+
+        }
+
+
+    }
+}
